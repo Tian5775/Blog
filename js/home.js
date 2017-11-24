51a9411d1cabf697a5e0bf387c41eae3672b33e6
pointAt-Tian <m18825070504@163.com>
2017-11-24 16:04:39 +0800

    20171124 pointat-Tian 优化首页

--- js/home.js
+++ js/home.js
@@ -1,4 +1,8 @@
 define(["app"],function(app){
     app.controller("home",function($scope){
-    })
+
+        $(window).on('scroll', function (event) {
+            console.log("ok");
+        });
+    });
 });
