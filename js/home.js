7fe293bb033ab79ca862fffb8e9670f5195bd355
pointAt-Tian <m18825070504@163.com>
2017-12-06 15:08:59 +0800

    20171206 pointat-Tian 优化页面切换

--- js/home.js
+++ js/home.js
@@ -1,30 +1,7 @@
 define(["app"],function(app){
     app.controller("home",function($scope){
         $scope.checkedPage = 0;
-        $scope.pageShow = [true,true,true,true];
-         /*$scope.pageChange = function($event, $delta, $deltaX, $deltaY){
-            //滚轮向下
-            if($deltaY > 0){
-                for(var i=0;i<$scope.pageShow.length-1;i++){
-                    if($scope.pageShow[i]){
-                        $scope.pageShow[i] = false;
-                        $scope.pageShow[i+1] = true;
-                        return;
-                    }
-                }
-            }
-            //滚轮向上
-            else {
-                for(var i=1;i<$scope.pageShow.length;i++){
-                    if($scope.pageShow[i]){
-                        $scope.pageShow[i] = false;
-                        $scope.pageShow[i -1] = true;
-                        return;
-                    }
-                }
-            }
-            return;
-        }*/
+        $scope.checkedPageCss = "checkedPage0"
 
         $scope.pageChange = function($event, $delta, $deltaX, $deltaY){
             if ($deltaY > 0 && $scope.checkedPage < 3){
@@ -32,13 +9,7 @@ define(["app"],function(app){
             } else if ($deltaY < 0 && $scope.checkedPage > 0){
                 $scope.checkedPage --;
             }
-            var allPage = angular.element(document.querySelector('.home .allPage '));
-            for(var i=0;i<4;i++){
-                if(allPage.hasClass("checkPage" + i)){
-                    allPage.removeClass("checkPage" + i);
-                }
-            }
-            allPage.addClass("checkPage" + $scope.checkedPage);
+            $scope.checkedPageCss = "checkedPage" + $scope.checkedPage;
         }
     });
 });
