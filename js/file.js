681a96f252f9696c4095b9237e80ebffb68f2844
pointAt-Tian <m18825070504@163.com>
2018-02-26 10:49:11 +0800

    20180226 pointat-Tian 编辑文件列表页面

--- js/file.js
+++ js/file.js
@@ -1,4 +1,20 @@
-define(["app"],function(app){
-    app.controller("file",function($scope){
+define(["app","jquery","editormd"],function(app){
+    app.controller("file",function($scope,$location,$http,$rootScope,$compile){
+        $scope.fileLoad = function(){
+            $http({
+                withCredentials: true,
+                method:"post",
+                headers:{'Content-Type': 'application/json'},
+                url:'http://' + $rootScope.url + ':8888/fileLoad'
+            }).then(
+                function successCallback(response){
+                    if(response.data.result == 1){
+
+                    }
+                },
+                function errorCallback(response){
+                }
+            );
+        }
     })
 });
